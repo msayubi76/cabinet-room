21243f68cbebc6333df56eb9a4ae026623712754
fitsolprojects <47750619+msayubi76@users.noreply.github.com>
2022-10-02 14:40:33 +0500

    Update sidebar.blade.php

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -65,8 +65,7 @@
                         <a class="has-arrow" href="javascript:void()"   aria-expanded="false">
                             <i class="icon-badge menu-icon"></i><span class="nav-text">Setting</span>
                         </a>
-                        <ul aria-expanded="false"> 
-                            
+                        <ul aria-expanded="false">
                             <li><a href="{{ url('admin/banners')}}"> Banners</a></li>
                             <li><a href="{{ url('admin/roles')}}">Role List</a></li>
                             <li><a href="{{ url('admin/permissions')}}">permission List</a></li>
