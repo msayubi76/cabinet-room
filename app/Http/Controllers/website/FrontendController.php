953613ce06549e72fa5623fd7b067788be524520
Ali Shahbaz <alishahbaz0313159@gmail.com>
2025-06-06 11:45:28 +0500

    commit1

--- app/Http/Controllers/website/FrontendController.php
+++ app/Http/Controllers/website/FrontendController.php
@@ -29,7 +29,7 @@ class FrontendController extends Controller
 
         $cart = Cart::where('user_id', Auth::id())->get();
         $banners = Banner::orderBy('id', 'DESC')->where('name', 'home')->get();
-        //$saleItems = Product::where('discount', '>', '0')->with(['category'])->limit(24)->latest()->where('is_active', '1')->limit(36)->get();
+        $saleItems = Product::where('discount', '>', '0')->with(['category'])->limit(24)->latest()->where('is_active', '1')->limit(36)->get();
 
         $categoriesWithProducts = $categories->filter((function ($category) {
             return $category->products()->count() > 0;
