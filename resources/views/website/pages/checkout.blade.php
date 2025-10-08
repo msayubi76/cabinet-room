e8ea02cdbc2c789ceaeb5097b0fb782b3ed77fd4
Ali Shahbaz <alishahbaz0313159@gmail.com>
2025-10-08 18:55:22 +0500

    dsdfm

--- resources/views/website/pages/checkout.blade.php
+++ resources/views/website/pages/checkout.blade.php
@@ -250,7 +250,8 @@
 
                                             <b>Account No</b><br>
                                             <span>PK86SONE0020520004454961</span><br>
-
+                                            
+                                            <b>Instructions</b><br>
                                             <p>After making the payment, please send your <b>payment slip</b> to us via the <b>WhatsApp chat button</b> below. Our team will verify your payment shortly and confirm your order for dispatch.</p>
                                         </div>
 
